c27cfd95c21774e42fd045481683302fd0b00dd5
Yash Agarwal <85528865+yashukun@users.noreply.github.com>
2023-09-14 14:04:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+import logging.handlers
+import os
+
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
+if __name__ == "__main__":
+    logger.info(f"Token value: {SOME_SECRET}")
+    response = requests.get('https://speedx-test.onrender.com/api/bookings/allbooking')
+    if response.status_code == 200:
+        data = response.json()
